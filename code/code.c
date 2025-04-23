89e5105bc4a7c2c15a3771e92d4d134af26d0b00
356dogs <didyblckm@gmail.com>
2025-04-23 18:19:34 +0200

    debut des fonctions de gestion de score

--- code/code.c
+++ code/code.c
@@ -4,7 +4,48 @@
 
 void afficherMenu()
 {
-    printf("+-----------------------------------+\n|1 : Addition                       |\n|2 : Soustraction                   |\n|3 : Multiplication                 |\n|4 : Tables des multiplications     |\n|5 : Divisions                      |\n|0 : Sortir du jeu                  |\n+-----------------------------------+\nQuel est votre choix ?\t");
+    printf("+-----------------------------------+\n|1 : Addition                       |\n|2 : Soustraction                   |\n|3 : Multiplication                 |\n|4 : Tables des multiplications     |\n|5 : Divisions                      |\n|0 : Sortir du jeu et               |\n|    enregistrer le score           |\n+-----------------------------------+\nQuel est votre choix ?\t");
+}
+
+void gestionReponse(int resultat, int *score)
+{
+    int essaie = 3; // 3 essaie
+    int reponse; // réponse de l'utilisateur
+
+    while (essaie > 0)
+    {
+        printf("Entrer le résultat\t");
+        scanf("%d", &reponse);
+        printf("Vous avez entré : %d\n", reponse);
+        if (reponse == resultat)
+        {
+            switch (essaie)
+            {
+                case 3:
+                    printf("Bravo, vous gagnez 10 points!\n\n");
+                    *score += 10; // incrémentation du score
+                    return;
+                case 2:
+                    printf("Bravo ! Vous avez utilisé 2 essais. Vous gagnez donc 5 points.\n\n");
+                    *score += 5; // incrémentation du score
+                    return;
+                case 1:
+                    printf("Bravo ! Vous avez utilisé 3 essai. Vous ne gagnez donc qu'un point.\n\n");
+                    *score += 1; // incrémentation du score
+                    return;
+            }
+        }
+        else
+        {
+            essaie--;
+            if (essaie == 0){
+                printf("Désolé, la bonne réponse est %d.\n\n", resultat);
+            }
+            else{
+                printf("Désolé, ce n'est pas la bonne réponse. Il vous reste %d essais.\n\n", essaie);
+            }
+        }
+    }
 }
 
 void addition(int *score)
@@ -14,66 +55,11 @@ void addition(int *score)
     int b = rand() % 101; // nombre aléatoire entre 0 et 100
     int resultat = a + b;
     int reponse;
+    int essaie = 3; // 3 essaie
 
-    printf("\nAddition :\n%d + %d = ?\nEntrer le résultat\t", a, b);
-    scanf("%d", &reponse);
-    printf("Vous avez entré : %d\n", reponse);
+    printf("\nAddition :\n%d + %d = ?\n", a, b);
 
-    if (reponse == resultat)
-    {
-        printf("Bravo !\n\n");
-        (*score)++; // incrémentation du score
-    }
-    else
-    {
-        printf("Désolé, la bonne réponse est %d.\n\n", resultat);
-    }
-}
-
-void multiplication(int *score)
-{
-    srand(time(NULL));
-    int a = rand() % 10 +1; // nombre aléatoire entre 1 et 10
-    int b = rand() % 10 +1; // nombre aléatoire entre 1 et 10
-    int resultat = a * b;
-    int reponse;
-
-    printf("\nMultiplication :\n%d * %d = ?\nEntrer le résultat\t", a, b);
-    scanf("%d", &reponse);
-    printf("Vous avez entré : %d\n", reponse);
-
-    if (reponse == resultat)
-    {
-        printf("Bravo !\n\n");
-        (*score)++; // incrémentation du score
-    }
-    else
-    {
-        printf("Désolé, la bonne réponse est %d.\n\n", resultat);
-    }
-}
-
-void division(int *score)
-{
-    srand(time(NULL));
-    int a = rand() % 20 + 1; // nombre aléatoire entre 1 et 20
-    int b = rand() % 9 + 2; // nombre aléatoire entre 2 et 10
-    int resultat = a; // le dividende
-    int reponse;
-
-    printf("\nDivision :\n%d / %d = ? (entier uniquement)\nEntrer le résultat\t", a * b, b);
-    scanf("%d", &reponse);
-    printf("Vous avez entré : %d\n", reponse);
-
-    if (reponse == a)
-    {
-        printf("Bravo !\n\n");
-        (*score)++; // incrémentation du score
-    }
-    else
-    {
-        printf("Désolé, la bonne réponse est %d.\n\n", a);
-    }
+    gestionReponse(resultat, score); // appel de la fonction pour gérer la réponse de l'utilisateur
 }
 
 void soustraction(int *score)
@@ -85,27 +71,46 @@ void soustraction(int *score)
     int min = (a > b) ? b : a;
     int resultat = max - min;
     int reponse;
+    int essaie = 3; // 3 essaie
 
-    printf("\nSoustraction :\n%d - %d = ?\nEntrer le résultat\t", max, min);
-    scanf("%d", &reponse);
-    printf("Vous avez entré : %d\n", reponse);
+    printf("\nSoustraction :\n%d - %d = ?\n", max, min);
 
-    if (reponse == resultat)
-    {
-        printf("Bravo !\n\n");
-        (*score)++; // incrémentation du score
-    }
-    else
-    {
-        printf("Désolé, la bonne réponse est %d.\n\n", resultat);
-    }
+    gestionReponse(resultat, score); // appel de la fonction pour gérer la réponse de l'utilisateur
 }
 
-void choixtables(int *score)
+void multiplication(int *score)
 {
-    int n; // largeur de la table
+    srand(time(NULL));
+    int a = rand() % 10 +1; // nombre aléatoire entre 1 et 10
+    int b = rand() % 10 +1; // nombre aléatoire entre 1 et 10
+    int resultat = a * b;
+    int reponse;
+    int essaie = 3; // 3 essaie
+
+    printf("\nMultiplication :\n%d * %d = ?\n", a, b);
+
+    gestionReponse(resultat, score); // appel de la fonction pour gérer la réponse de l'utilisateur
+}
+
+void division(int *score)
+{
+    srand(time(NULL));
+    int a = rand() % 20 + 1; // nombre aléatoire entre 1 et 20
+    int b = rand() % 9 + 2; // nombre aléatoire entre 2 et 10
+    int resultat = a; // le dividende
+    int reponse;
+    int essaie = 3; // 3 essaie
+
+    printf("\nDivision :\n%d / %d = ? (entier uniquement)\n", a * b, b);
+
+    gestionReponse(resultat, score); // appel de la fonction pour gérer la réponse de l'utilisateur
+}
+
+void choixTables(int *score)
+{
+    int n; // taille de la table
     int reponse; // réponse de l'utilisateur
-    
+
     printf("Vous avez choisi les tables des multiplications.\nSur quelle table voulez-vous travailler (entrez la valeur maximale. Exemple : 10 pour une table de 1 à 10)\t");
     scanf("%d", &n);
 
@@ -127,10 +132,10 @@ void choixtables(int *score)
         }
         printf("\n");
     }
-    printf("Voulez vous remplir une colonne par vous même ? (1=Oui,0=Non)\t"); 
+    printf("Voulez vous remplir une colonne par vous même ? (1=Oui,0=Non)\t");
     scanf("%d", &reponse); // lecture de la réponse de l'utilisateur
     if (reponse == 1)
-    {}
+    {
         int colonne; // numéro de la colonne à remplir
         printf("Entrez le numéro de la colonne (1 à %d)\t", n);
         scanf("%d", &colonne); // lecture du numéro de la colonne
@@ -157,68 +162,106 @@ void choixtables(int *score)
                 printf("Désolé, la bonne réponse est %d.\n", i * colonne);
             }
         }
+    }
 }
 
-void enregistrerScore(const char *nom, int score) {
-    FILE *fichier = fopen("score/scores.csv", "a"); // Ouvrir en mode ajout
-    if (fichier == NULL) {
-        printf("Erreur lors de l'ouverture du fichier des scores.\n");
+void menuEnregistrement()
+{
+    char choix;
+    char nom[50];
+
+    while (1==1)
+    {
+        printf("Bienvenue dans le jeu Champion des Maths !\n");
+        printf("+-----------------------------------+\n|1 : Se connecter                   |\n|   (avec un nom d'utilisateur)     |\n|2 : ne pas se connecter            |\n+-----------------------------------+\nQuel est votre choix ?\t");
+        scanf(" %c", &choix);
+
+        while (getchar() != '\n');
+
+        switch (choix)
+        {
+            case '1':
+                printf("Entrez votre nom d'utilisateur :\t");
+                scanf("%s", nom); // lecture du nom d'utilisateur
+                return; // sortie de la fonction
+            case '2':
+                printf("Entrez votre nom d'utilisateur : (ce nom doit être unique)\t");
+                scanf("%s", nom); // lecture du nom d'utilisateur
+                return; // sortie de la fonction
+            default:
+                printf("Choix invalide. Veuillez réessayer.\n");
+        }
+        
+    }
+}
+
+void lectureScore()
+{
+    FILE *fichier = fopen("score.txt","r"); // ouverture du fichier en mode lecture
+    if (fichier == NULL){
+        printf("Erreur d'ouverture du fichier.\n");
         return;
     }
 
-    // Obtenir la date et l'heure actuelles
-    time_t t = time(NULL);
-    struct tm tm = *localtime(&t);
 
-    // Écrire les informations dans le fichier
-    fprintf(fichier, "Nom: %s | Score: %d | Date: %02d/%02d/%04d %02d:%02d:%02d\n",
-            nom, score, tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900,
-            tm.tm_hour, tm.tm_min, tm.tm_sec);
 
-    fclose(fichier);
+}
+
+int enregistrerScore() {
+    FILE *fichier = fopen("score.txt","a");
+    if (fichier == NULL){
+        printf("Erreur d'ouverture du fichier.\n");
+        return 0;
+    }
+
+
 }
 
 int main()
 {
     char nom[50]; // tableau pour stocker le nom de l'utilisateur
-    int choix;
+    char choix;
     int score = 0; // initialisation du score
+    int peutquitter; // variable pour savoir si l'utilisateur peut quitter le jeu
 
-    printf("Entrez votre nom : ");
-    scanf("%s", nom);
+    menuEnregistrement(); // appel de la fonction pour afficher le menu d'enregistrement et la gestion du choix du joueur
 
     afficherMenu();
-    scanf("%d", &choix);
+    scanf(" %c", &choix);
 
-    while (choix != 0)
+    while (choix != '0')
     {
+        while (getchar() != '\n');
         switch (choix)
         {
-            case 1:
+            case '1':
                 addition(&score);
                 break;
-            case 2:
+            case '2':
                 soustraction(&score);
                 break;
-            case 3:
+            case '3':
                 multiplication(&score);
                 break;
-            case 4:
-                choixtables(&score);
+            case '4':
+                choixTables(&score);
                 break;
-            case 5:
+            case '5':
                 division(&score);
                 break;
+            case '0':
+                printf("Merci d'avoir joué ! Votre score final est : %d\n", score); // affichage du score final
+                peutquitter = enregistrerScore(); // enregistrement du score dans le fichier
+                if (peutquitter == 1){ //true
+                    return 0;
+                }
             default:
-                printf("Choix invalide. Veuillez réessayer.\n");
+                printf("Choix invalide. Veuillez réessayer.\n\n");
         }
-
+        choix = 0;
         printf("Score actuel : %d\n", score); // affichage du score actuel
         afficherMenu();
-        scanf("%d", &choix);
+        scanf(" %c", &choix);
+        printf("");
     }
-
-    printf("Merci d'avoir joué ! Votre score final est : %d\n", score); // affichage du score final
-    enregistrerScore(nom, score); // enregistrement du score dans le fichier
-    return 0;
 }
