482a4c2ca73fb7169c0de297429971a43ee499c9
356dogs <didyblckm@gmail.com>
2025-04-24 22:22:14 +0200

    finition des fonction de gestion du score

--- code/code.c
+++ code/code.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 void afficherMenu()
 {
-    printf("+-----------------------------------+\n|1 : Addition                       |\n|2 : Soustraction                   |\n|3 : Multiplication                 |\n|4 : Tables des multiplications     |\n|5 : Divisions                      |\n|0 : Sortir du jeu et               |\n|    enregistrer le score           |\n+-----------------------------------+\nQuel est votre choix ?\t");
+    printf("+-----------------------------------+\n|1 : Addition                       |\n|2 : Soustraction                   |\n|3 : Multiplication                 |\n|4 : Tables des multiplications     |\n|5 : Divisions                      |\n|0 : Sortir du jeu et               |\n|    enregistrer le score           |\n+-----------------------------------+\nQuel est votre choix ?  ");
 }
 
 void gestionReponse(int resultat, int *score)
@@ -165,15 +166,14 @@ void choixTables(int *score)
     }
 }
 
-void menuEnregistrement()
+char menuEnregistrement(int *nom,int *score, int lignejoeur) // fonction pour afficher le menu d'enregistrement et gérer le choix du joueur
 {
     char choix;
-    char nom[50];
 
     while (1==1)
     {
         printf("Bienvenue dans le jeu Champion des Maths !\n");
-        printf("+-----------------------------------+\n|1 : Se connecter                   |\n|   (avec un nom d'utilisateur)     |\n|2 : ne pas se connecter            |\n+-----------------------------------+\nQuel est votre choix ?\t");
+        printf("+-----------------------------------+\n|1 : Se connecter                   |\n|   (avec un nom d'utilisateur)     |\n|2 : ne pas se connecter            |\n+-----------------------------------+\nQuel est votre choix ?  ");
         scanf(" %c", &choix);
 
         while (getchar() != '\n');
@@ -181,13 +181,26 @@ void menuEnregistrement()
         switch (choix)
         {
             case '1':
-                printf("Entrez votre nom d'utilisateur :\t");
+                printf("Entrez votre nom d'utilisateur :  ");
                 scanf("%s", nom); // lecture du nom d'utilisateur
+                // appel de la fonction pour lire le score
+                if (lectureScore(nom,score,lignejoeur) == 0) // si le score n'est pas trouvé
+                {
+                    printf("Erreur, le score n'a pas pu être trouvé (verifiez bien que le nom d'utilisateur soit le même).\n\n");
+                    break; // sortie de la boucle                    
+                }
                 return; // sortie de la fonction
             case '2':
-                printf("Entrez votre nom d'utilisateur : (ce nom doit être unique)\t");
-                scanf("%s", nom); // lecture du nom d'utilisateur
-                return; // sortie de la fonction
+                do {
+                    printf("Entrez votre nom d'utilisateur : (ce nom doit être unique)  ");
+                    scanf("%s", nom); // lecture du nom d'utilisateur
+                    if (verifJoueur(nom) == 1) {
+                        printf("Ce nom d'utilisateur existe déjà. Veuillez en choisir un autre.\n");
+                    }
+                } while (verifJoueur(nom) == 1);
+                
+                *score = 0; // initialisation du score à 0
+                return choix; // sortie de la fonction
             default:
                 printf("Choix invalide. Veuillez réessayer.\n");
         }
@@ -195,26 +208,109 @@ void menuEnregistrement()
     }
 }
 
-void lectureScore()
-{
-    FILE *fichier = fopen("score.txt","r"); // ouverture du fichier en mode lecture
-    if (fichier == NULL){
+int verifJoueur(char *nom_joueur) {    
+    FILE *fichier = fopen("score/score.txt", "r");
+    if (fichier == NULL) {
+        printf("Erreur d'ouverture du fichier.\n");
+        return 1;
+    }
+
+    char ligne[256];
+    int nom_trouve = 0;
+
+    while (fgets(ligne, sizeof(ligne), fichier)) {
+        // Créer une copie de la ligne car strtok modifie la chaîne
+        char ligne_copy[256];
+        strcpy(ligne_copy, ligne);
+        
+        // Récupère uniquement le nom (première partie avant la virgule)
+        char *nom = strtok(ligne_copy, ",");
+
+        if (strcmp(nom, nom_joueur) == 0) {
+            nom_trouve = 1;
+            break;
+        }
+    }
+
+    fclose(fichier);
+    return nom_trouve; // retourne 1 si le nom existe déjà, 0 sinon
+}
+
+// Fonction pour lire le score d'un joueur dans le fichier "score.txt"
+int lectureScore(char *nom_joueur, int *score_joueur, int *lignejoueur) {    
+    FILE *fichier = fopen("score/score.txt", "r");
+    if (fichier == NULL) {
         printf("Erreur d'ouverture du fichier.\n");
         return;
     }
 
+    char ligne[256];
+    int score_trouve = 0;
+    int ligne_courante = 0;  // Compteur de lignes
 
+    while (fgets(ligne, sizeof(ligne), fichier)) {
+        // Créer une copie de la ligne car strtok modifie la chaîne
+        char ligne_copy[256];
+        strcpy(ligne_copy, ligne);
+        
+        // Séparer la ligne en éléments
+        char *nom = strtok(ligne_copy, ",");// Récupère le nom
+        char *score_str = strtok(NULL, ",");// Récupère le score
+        char *date = strtok(NULL, "\n");// Récupère la date
 
+        if (nom && score_str && date) {
+            if (strcmp(nom, nom_joueur) == 0) {
+                *score_joueur = atoi(score_str); // Convertit le score en entier
+                printf("Score trouvé pour %s le %s : %d\n", nom_joueur, date, *score_joueur);
+                score_trouve = 1;
+                break;
+            }
+        }
+        ligne_courante++; // Incrémente le compteur de lignes
+    }
+    *lignejoueur = ligne_courante; // Met à jour la ligne du joueur 
+
+    fclose(fichier);
+    return score_trouve; // retourne 1 si le score a été trouvé, sinon 0
 }
 
-int enregistrerScore() {
-    FILE *fichier = fopen("score.txt","a");
-    if (fichier == NULL){
-        printf("Erreur d'ouverture du fichier.\n");
-        return 0;
+int enregistrerScore(char *nom_joueur, int score,int lignejoueur) {
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    if (lignejoueur == -1) // Si le joueur n'existe pas dans le fichier
+    {
+        FILE *fichier = fopen("score/score.txt", "a"); // Ouvre le fichier en mode ajout
+        if (fichier == NULL) {
+            printf("Erreur d'ouverture du fichier.\n");
+            return 0;
+        }
+        
+        fprintf(fichier,"%s,%d,%02d/%02d/%04d à %02dh%02d\n",nom_joueur, score,tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900,tm.tm_hour, tm.tm_min);
+        fclose(fichier);
     }
+    else // Si le joueur existe déjà dans le fichier
+    {
+        FILE *fichier = fopen("score/score.txt", "r+"); // Ouvre le fichier en mode lecture/écriture
+        if (fichier == NULL) {
+            printf("Erreur d'ouverture du fichier.\n");
+            return 0;
+        }
+        
+        char ligne[256];
+        int ligne_courante = 0;  // Compteur de lignes
 
+        while (fgets(ligne, sizeof(ligne), fichier)) {
+            if (ligne_courante == lignejoueur) { // Si on est à la ligne du joueur
+                fseek(fichier, -strlen(ligne)-1, SEEK_CUR); // Reculer à la position de la ligne actuelle
+                fprintf(fichier,"%s,%d,%02d/%02d/%04d à %02dh%02d\n",nom_joueur,score,tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900,tm.tm_hour, tm.tm_min);
+                fclose(fichier);
+                return 1; // Score mis à jour avec succès
+            }
+            ligne_courante++; // Incrémente le compteur de lignes
+        }
 
+        fclose(fichier);
+    }
 }
 
 int main()
@@ -223,15 +319,18 @@ int main()
     char choix;
     int score = 0; // initialisation du score
     int peutquitter; // variable pour savoir si l'utilisateur peut quitter le jeu
+    int lignejoueur = -1; // variable pour stocker a quelle ligne se trouve le joueur dans le fichier score.txt (-1 veut dire a la fin)
 
-    menuEnregistrement(); // appel de la fonction pour afficher le menu d'enregistrement et la gestion du choix du joueur
+    menuEnregistrement(nom,&score,&lignejoueur); // appel de la fonction pour afficher le menu d'enregistrement et la gestion du choix du joueur
 
     afficherMenu();
     scanf(" %c", &choix);
 
-    while (choix != '0')
+    while (1)
     {
         while (getchar() != '\n');
+        {
+
         switch (choix)
         {
             case '1':
@@ -250,18 +349,21 @@ int main()
                 division(&score);
                 break;
             case '0':
-                printf("Merci d'avoir joué ! Votre score final est : %d\n", score); // affichage du score final
-                peutquitter = enregistrerScore(); // enregistrement du score dans le fichier
-                if (peutquitter == 1){ //true
+                printf("Merci d'avoir joué %s ! Votre score final est : %d\n", nom, score);
+                if (enregistrerScore(nom, score,lignejoueur)) {
+                    printf("Score enregistré avec succès !\n");
                     return 0;
+                } else {
+                    printf("Erreur lors de l'enregistrement du score.\n");
+                    return 1;
                 }
             default:
                 printf("Choix invalide. Veuillez réessayer.\n\n");
         }
-        choix = 0;
-        printf("Score actuel : %d\n", score); // affichage du score actuel
+        printf("Score actuel de %s : %d\n", nom, score); // affichage du score actuel
         afficherMenu();
         scanf(" %c", &choix);
         printf("");
+    }
     }
 }
